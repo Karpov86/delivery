9896e1826577a6488709a00b6667695157b2a010
Valery Karpov <karpoff19@mail.ru>
2020-07-24 15:25:09 +0300

    Create repositories

--- database/src/main/java/by/karpov/delivery/repository/DishRepository.java
+++ database/src/main/java/by/karpov/delivery/repository/DishRepository.java
@@ -1,9 +1,14 @@
 package by.karpov.delivery.repository;
 
+import by.karpov.delivery.entity.Category;
 import by.karpov.delivery.entity.Dish;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Transactional
 public interface DishRepository extends JpaRepository<Dish, Long> {
+
+    List<Dish> findAllByCategory(Category category);
 }
