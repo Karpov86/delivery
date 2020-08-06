3e4ef655e6d97a11198ebc87f0cc232bf4452b3b
Valery Karpov <karpoff19@mail.ru>
2020-08-06 11:38:33 +0300

    add service for AddressService

--- service/src/main/java/by/karpov/delivery/service/AddressService.java
+++ service/src/main/java/by/karpov/delivery/service/AddressService.java
@@ -1,4 +1,11 @@
 package by.karpov.delivery.service;
 
+import by.karpov.delivery.entity.Address;
+
 public interface AddressService {
+
+    Address getById(Long id);
+    Address save(Address address);
+    void delete(Long id);
+    void deleteByAddress(Address address);
 }
