3a3433079a6df836102b6076b9c7bb35c45192c9
Valery Karpov <karpoff19@mail.ru>
2020-08-11 11:04:32 +0300

    add

---
+++ web/src/main/java/by/karpov/delivery/controller/OrderController.java
@@ -0,0 +1,96 @@
+package by.karpov.delivery.controller;
+
+import by.karpov.delivery.entity.Dish;
+import by.karpov.delivery.entity.Order;
+import by.karpov.delivery.entity.User;
+import by.karpov.delivery.service.DishService;
+import by.karpov.delivery.service.OrderService;
+import by.karpov.delivery.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpSession;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+@Controller
+@RequestMapping("order")
+public class OrderController {
+
+    private final UserService userService;
+    private final OrderService orderService;
+    private final DishService dishService;
+
+    @Autowired
+    public OrderController(UserService userService, OrderService orderService, DishService dishService) {
+        this.userService = userService;
+        this.orderService = orderService;
+        this.dishService = dishService;
+    }
+
+    @ModelAttribute("currentUser")
+    public User getCurrentUser() {
+        return userService.getUserFromSecurityContext();
+    }
+
+    @ModelAttribute("currentOrder")
+    public Order getCurrentOrder() {
+        return orderService.getLast(getCurrentUser());
+        //return getCurrentUser().getOrder();
+    }
+
+    @ModelAttribute("newDish")
+    public Dish getNewDish() {
+        return new Dish();
+    }
+
+    @GetMapping
+    public String getOrder(Model model) {
+        BigDecimal totalPrice = orderService.getTotalPrice(getCurrentOrder());
+        model.addAttribute("totalPrice", totalPrice);
+        return "order";
+    }
+
+    @PostMapping("/add")
+    public String addDish(@RequestParam("dishId") Long id) {
+        //Order order = getCurrentUser().getOrder();
+        Order order = orderService.getLast(getCurrentUser());
+        Dish dish = dishService.getById(id);
+        order.getDishes().add(dish);
+        orderService.save(order);
+        return "redirect:/home/dishes?category=" + dish.getCategory();
+    }
+
+    @PostMapping("/delete")
+    public String deleteDish(@RequestParam Long id){
+        Order order = getCurrentOrder();
+        Dish dish = order.getDishes().stream()
+                .filter(d -> d.getId().equals(id))
+                .findFirst()
+                .get();
+        order.getDishes().remove(dish);
+        orderService.save(order);
+        return "redirect:/order";
+    }
+
+    @PostMapping("/submit")
+    public String submitOrder(){
+        User user = getCurrentUser();
+        if (orderService.getLast(user).getDishes().isEmpty()){
+            return "redirect:/order";
+        }
+        Order order = Order.builder()
+                .dishes(new ArrayList<>(Collections.emptyList()))
+                .user(user)
+                .dateTime(LocalDateTime.now())
+                .build();
+        orderService.save(order);
+        user.setOrder(order);
+        return "redirect:/home";
+    }
+}
