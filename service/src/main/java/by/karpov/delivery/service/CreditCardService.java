dd0b94801e4e11927ad7a53c3cff542fa9bb78e4
Valery Karpov <karpoff19@mail.ru>
2020-08-11 10:56:10 +0300

    refactoring

--- service/src/main/java/by/karpov/delivery/service/CreditCardService.java
+++ service/src/main/java/by/karpov/delivery/service/CreditCardService.java
@@ -1,4 +1,14 @@
 package by.karpov.delivery.service;
 
+import by.karpov.delivery.entity.CreditCard;
+
 public interface CreditCardService {
+
+    CreditCard getById(Long id);
+
+    CreditCard save(CreditCard creditCard);
+
+    void delete(Long id);
+
+    void deleteByCreditCard(CreditCard creditCard);
 }
