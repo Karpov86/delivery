2dd5d7fc2fb3f96dec4e7df28051dd8b6fc55ff2
Valery Karpov <karpoff19@mail.ru>
2020-08-11 11:03:27 +0300

    add crud services

--- service/src/main/java/by/karpov/delivery/service/impl/CreditCardServiceImpl.java
+++ service/src/main/java/by/karpov/delivery/service/impl/CreditCardServiceImpl.java
@@ -1,4 +1,38 @@
 package by.karpov.delivery.service.impl;
 
-public class CreditCardServiceImpl {
+import by.karpov.delivery.entity.CreditCard;
+import by.karpov.delivery.repository.CreditCardRepository;
+import by.karpov.delivery.service.CreditCardService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CreditCardServiceImpl implements CreditCardService {
+
+    private final CreditCardRepository creditCardRepository;
+
+    @Autowired
+    public CreditCardServiceImpl(CreditCardRepository creditCardRepository) {
+        this.creditCardRepository = creditCardRepository;
+    }
+
+    @Override
+    public CreditCard getById(Long id) {
+        return creditCardRepository.getOne(id);
+    }
+
+    @Override
+    public CreditCard save(CreditCard creditCard) {
+        return creditCardRepository.save(creditCard);
+    }
+
+    @Override
+    public void delete(Long id) {
+        creditCardRepository.deleteById(id);
+    }
+
+    @Override
+    public void deleteByCreditCard(CreditCard creditCard) {
+        creditCardRepository.delete(creditCard);
+    }
 }
