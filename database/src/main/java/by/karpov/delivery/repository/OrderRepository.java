adc6d9667e2c309fcf82eee2c249edb724d5e7f2
Valery Karpov <karpoff19@mail.ru>
2020-08-11 11:05:00 +0300

    refactoring

--- database/src/main/java/by/karpov/delivery/repository/OrderRepository.java
+++ database/src/main/java/by/karpov/delivery/repository/OrderRepository.java
@@ -5,7 +5,10 @@ import by.karpov.delivery.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Transactional
 public interface OrderRepository extends JpaRepository<Order, Long> {
     Order findByUser(User user);
+    List<Order> findAllByUser (User user);
 }
