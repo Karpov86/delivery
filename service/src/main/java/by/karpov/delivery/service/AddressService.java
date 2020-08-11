e25e7fcc1d36d8527cfd1b8c3c967f6048ca8e84
Valery Karpov <karpoff19@mail.ru>
2020-08-11 10:55:04 +0300

    refactoring

--- service/src/main/java/by/karpov/delivery/service/AddressService.java
+++ service/src/main/java/by/karpov/delivery/service/AddressService.java
@@ -5,7 +5,10 @@ import by.karpov.delivery.entity.Address;
 public interface AddressService {
 
     Address getById(Long id);
+
     Address save(Address address);
+
     void delete(Long id);
+
     void deleteByAddress(Address address);
 }
